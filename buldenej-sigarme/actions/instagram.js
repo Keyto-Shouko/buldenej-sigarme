f118f92630aa022d9abbfa25e5576e44136c6689
Keyto <balvesca@gmail.com>
2024-08-26 13:49:54 +0200

    will try to use github actions to do the job

--- buldenej-sigarme/actions/instagram.js
+++ buldenej-sigarme/actions/instagram.js
@@ -1,10 +1,14 @@
 import fetch from 'node-fetch';
-import { supabaseServer } from '../src/supabaseServer.js';
+import { createClient } from '@supabase/supabase-js';
 
-export default async function handler(req, res) {
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+const instagramApiUrl = process.env.INSTAGRAM_API_URL;
+
+async function updateInstagramPost() {
   try {
-    // Fetch latest Instagram post
-    const response = await fetch('https://instagram.com/graphql/query/?query_id=17888483320059182&variables={"id":"17825641829","first":1,"after":null}');
+    const response = await fetch(instagramApiUrl);
     if (!response.ok) {
       throw new Error(`Instagram API error: ${response.statusText}`);
     }
@@ -17,20 +21,20 @@ export default async function handler(req, res) {
 
     const postUrl = `https://www.instagram.com/p/${latestPost.shortcode}/`;
 
-    // Update the first row in the buldenejInsta table with the latest post URL
-    const { error } = await supabaseServer
+    const { error } = await supabase
       .from('buldenejInsta')
       .update({ url: postUrl })
       .eq('id', 1);
 
     if (error) {
       console.error('Error updating Instagram posts:', error);
-      return res.status(500).json({ error: 'Error updating Instagram posts', details: error });
+      return;
     }
 
-    res.status(200).json({ message: 'Latest post updated successfully' });
+    console.log('Latest post updated successfully');
   } catch (error) {
     console.error('Error updating Instagram posts:', error);
-    return res.status(500).json({ error: 'Error updating Instagram posts', details: error.message });
   }
 }
+
+updateInstagramPost();
